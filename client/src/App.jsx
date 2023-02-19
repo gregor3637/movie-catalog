43a58cba2e79b6783aef316ef2bbd2e03b8c3374
Mihail Mitov <mihail.mitov@droxic.com>
2023-02-20 01:26:44 +0200

    [client] theme config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import themeConfigs from "./config/theme.configs";
+import { ThoastContainer } from "react-toastify";
+import { CssBaseline } from "@mui/material/CssBaseline";
+
 const App = () => {
-  return <div>App</div>;
+  const { themeMode } = useSelector((state) => state.themeMode);
+  return (
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* config toastify */}
+      <ThoastContainer
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+        position="bottom-left"
+      />
+
+      {/* mui reset css */}
+      <CssBaseline />
+      App
+    </ThemeProvider>
+  );
 };
 
 export default App;
