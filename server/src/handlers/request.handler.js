fe42991708ca6af5300582d109159e2561757d0c
Mihail Mitov <mihail.mitov@droxic.com>
2023-02-19 19:55:28 +0200

    [server] handlers

---
+++ server/src/handlers/request.handler.js
@@ -0,0 +1,9 @@
+import { validateionResult } from "express-validator";
+
+const validate = (req, res, next) => {
+  const errors = validateionResult(req);
+
+  if (!errors.isEmpty()) return res.status(400).json(errors.array()[0].msg);
+};
+
+export default { validate };
