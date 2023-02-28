8888711cdf9db6f2c5c94b2a70686f62b77cf7d7
Mihail Mitov <mihail.mitov@droxic.com>
2023-03-01 00:49:31 +0200

    [server] remove logs

--- server/src/handlers/request.handler.js
+++ server/src/handlers/request.handler.js
@@ -1,9 +1,7 @@
 import { validationResult } from "express-validator";
 
 const validate = (req, res, next) => {
-  console.log("🚀 ~ file: request.handler.js:4 ~ validate ~ req:")
   const errors = validationResult(req);
-  console.log("🚀 ~ file: request.handler.js:6 ~ validate ~ errors:", errors)
 
   if (!errors.isEmpty()) return res.status(400).json(errors.array()[0].msg);
 
